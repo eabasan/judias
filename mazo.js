c7e64b4520adab2a80dc9335cf6be1d7991c2713
eabasan <elenaabato2@gmail.com>
2025-08-15 16:31:34 +0200

    Actualizacion sin botones

--- mazo.js
+++ mazo.js
@@ -1,3 +1,4 @@
+// mazo.js
 export const judias = [
   { nombre: "Judía Boom", cantidad: 18 },
   { nombre: "La Pocha", cantidad: 14 },
@@ -30,6 +31,7 @@ export function barajar(array) {
 export function repartirCartas(mazo, cantidad = 5) {
   const mano = [];
   for (let i = 0; i < cantidad; i++) {
+    if(mazo.length === 0) break;
     mano.push(mazo.pop());
   }
   return mano;
